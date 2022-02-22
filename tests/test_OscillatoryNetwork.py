94e9c53fe19c7f62909e55775d7eea8159fa0a95
Mariia Turchina <mariia.turchina@gmail.com>
2022-02-22 22:25:48 +0100

    added some tests for network funcs

--- tests/test_OscillatoryNetwork.py
+++ tests/test_OscillatoryNetwork.py
@@ -2,4 +2,48 @@ from src.OscillatoryNetwork import *
 
 class TestOscillatoryNetwork:
 
-    pass
+    network1 = OscillatoryNetwork(
+        nr_excitatory=2,
+        nr_inhibitory=2,
+        nr_oscillators=1
+    )
+
+    network2 = OscillatoryNetwork(
+        nr_excitatory=8,
+        nr_inhibitory=4,
+        nr_oscillators=4
+    )
+
+    def test_change_recovery(self):
+
+        d_recovery1 = self.network1._change_recovery()
+        d_recovery1_expected = [0, 0, 0, 0]
+
+        # -----------------------------------------------------------------------
+
+        d_recovery2 = self.network2._change_recovery()
+        d_recovery2_expected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        assert np.array_equal(d_recovery1, d_recovery1_expected)
+        assert np.array_equal(d_recovery2, d_recovery2_expected)
+
+    def test_change_potential(self):
+
+        c1 = [0.1] * 4
+
+        d_potential1 = self.network1._change_potential(current=c1)
+        d_potential1_expected = [-1.45] * 4
+
+        # -----------------------------------------------------------------------
+
+        c2 = [0.1] * 12
+
+        d_potential2 = self.network2._change_potential(current=c2)
+        d_potential2_expected = [-1.45] * 12
+
+        assert np.array_equal(d_potential1, d_potential1_expected)
+        assert np.array_equal(d_potential2, d_potential2_expected)
+
+    def test_change_synaptic_potentials(self):
+        # TODO
+        pass

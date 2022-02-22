7eba3ef4033b5e70a347dd591edf313e733979ba
Mariia Turchina <mariia.turchina@gmail.com>
2022-02-22 20:07:56 +0100

    added more misc tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,4 +1,5 @@
 from src.misc import *
+from src.NeuronTypes import *
 
 class TestMisc:
 
@@ -6,3 +7,7 @@ class TestMisc:
         assert euclidian_dist_R2((0, 0), (0, 4)) == 4
         assert round(euclidian_dist_R2((-7.3, -4), (17, 6.5)), 4) == 26.4715
         assert round(euclidian_dist_R2((-0.27, 3.91), (8.4, -0.5)), 4) == 9.7271
+
+    def test_neuron_slice(self):
+        assert neur_slice(NeuronTypes.E, 10, 5) == slice(10)
+        assert neur_slice(NeuronTypes.I, 10, 5) == slice(10, 15)

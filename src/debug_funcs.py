c3d8be1e7f32038390e2047358136d38d099281f
Mariia Turchina <mariia.turchina@gmail.com>
2022-05-24 02:46:19 +0200

    added an option to load stimulus data

---
+++ src/debug_funcs.py
@@ -0,0 +1,26 @@
+from src.stimulus_construction.StimulusFactory import *
+
+
+import numpy as np
+from os.path import exists
+from os import makedirs
+
+
+def try_pulling_stimulus_data(params_gabor, params_rf, params_ping, params_izhi, params_freqs):
+    folder_path = f"../local_storage/stimulus/{params_ping.nr_ping_networks}/"
+    if exists(folder_path):
+        stimulus_currents = np.loadtxt(folder_path + "currents.txt")
+        cortical_distances = np.loadtxt(folder_path + "distances.txt").reshape(
+            (params_ping.nr_ping_networks, params_ping.nr_ping_networks)
+        )
+    else:
+        print("Stimulus data does not exist. Creating stimulus data.")
+        stimulus = StimulusFactory().create(params_gabor, params_rf, params_ping, params_izhi, params_freqs)
+        stimulus_currents = stimulus.stimulus_currents
+        cortical_distances = stimulus.extract_stimulus_location().cortical_distances
+
+        makedirs(folder_path)
+        np.savetxt(folder_path + "currents.txt", stimulus_currents)
+        np.savetxt(folder_path + "distances.txt", cortical_distances)
+
+    return stimulus_currents, cortical_distances
--- src/main.py
+++ src/main.py
@@ -4,24 +4,32 @@ from src.izhikevich_simulation.ConnectivityGridPINGFactory import *
 from src.izhikevich_simulation.CurrentComponentsGridPING import *
 from src.izhikevich_simulation.IzhikevichNetworkSimulator import *
 from src.SpikingFrequencyComputer import *
+from src.debug_funcs import *
+
+DEBUGMODE = True
+
 
 if __name__ == "__main__":
     params_initializer = ParamsInitializer()
     params_ping, params_gabor, params_rf, params_connectivity, params_izhi, params_synaptic, params_freqs = params_initializer.initialize()
 
-    stimulus = StimulusFactory().create(params_gabor, params_rf, params_ping, params_izhi, params_freqs)
+    if DEBUGMODE:
+        stimulus_currents, cortical_distances = try_pulling_stimulus_data(params_gabor, params_rf, params_ping, params_izhi, params_freqs)
+    else:
+        stimulus = StimulusFactory().create(params_gabor, params_rf, params_ping, params_izhi, params_freqs)
 
-    stimulus_locations = stimulus.extract_stimulus_location()
+        stimulus_currents = stimulus.stimulus_currents
+        cortical_distances = stimulus.extract_stimulus_location().cortical_distances
 
     connectivity = ConnectivityGridPINGFactory().create(
         params_ping=params_ping,
         params_connectivity=params_connectivity,
-        cortical_distances=stimulus_locations.cortical_distances
+        cortical_distances=cortical_distances
     )
     neural_model = CurrentComponentsGridPING(
         connectivity=connectivity,
         params_synaptic=params_synaptic,
-        stimulus_currents=stimulus.stimulus_currents
+        stimulus_currents=stimulus_currents
     )
     simulation_outcome = IzhikevichNetworkSimulator(
         params_izhi=params_izhi,

95fa26ebdcf99614de7a223a25289ce13b59ce41
Mariia Turchina <mariia.turchina@gmail.com>
2022-11-04 01:15:55 +0100

    added args

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from src.Application import Application
 import os
-
+import argparse
 
 if __name__ == "__main__":
-    os.chdir("../")
+
+    parser = argparse.ArgumentParser(prog='grid-ping')
+    parser.add_argument('--root', action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+
+    if args.root is None:
+        os.chdir("../")
     Application().run()
 
 
